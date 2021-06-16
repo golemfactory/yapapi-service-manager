7bf8ac21811e2539c4478f2a6f30ddebfff533a6
Jan Betley <jan.betley@golem.network>
2021-06-16 11:24:56 +0200

    minor fixes in examples/clock.py

--- examples/clock.py
+++ examples/clock.py
@@ -10,7 +10,7 @@ class ProviderClock(Service):
     @classmethod
     async def get_payload(cls):
         #   We use a "standard" yapapi blender image (because every provider has it downloaded)
-        #   Could be any other working image, because this Service does exactly nothing.
+        #   Could be any other working image, the only requirement is /bin/date command
         image_hash = "9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae"
         return await vm.repo(image_hash=image_hash)
 
@@ -27,7 +27,7 @@ class ProviderClock(Service):
             #   but this particular operation is always last
             this_result = all_results[-1]
 
-            print("My time is", this_result.stdout)
+            print(f"My name {self.provider_name} and my time is {this_result.stdout}")
             await asyncio.sleep(1)
 
 
@@ -39,14 +39,20 @@ async def main(service_manager):
         await asyncio.sleep(1)
 
     print("Hey provider, what's the time?")
-    await asyncio.sleep(10)
+    await asyncio.sleep(7)
+
     print("OK, that's enough, thanks buddy")
-    await clock.stop()
+    clock.stop()
+
     await service_manager.close()
 
 
 if __name__ == '__main__':
-    service_manager = ServiceManager()
+    executor_cfg = {
+        'subnet_tag': 'devnet-beta.2',
+        'budget': 1,
+    }
+    service_manager = ServiceManager(executor_cfg)
     try:
         loop = asyncio.get_event_loop()
         main_task = loop.create_task(main(service_manager))
