685bd1714f1ee71719619364342cdabd08de6930
Pawel Nowosielski <pnowosie@users.noreply.github.com>
2021-06-18 14:06:55 +0200

    create a Python package with service manager (#4)

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yapapi-service-manager",
+    version="0.0.0",
+    author="Golem Factory, Jan Betley",
+    author_email="contact@golem.network, jan.betley@golem.network",
+    description="Helper tool for management of Golem-based services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://handbook.golem.network/yapapi/",
+    download_url="https://github.com/golemfactory/yapapi-service-manager",
+    packages=setuptools.find_packages(),
+    install_requires=["yapapi==0.6.0",],
+    classifiers=[
+        "Development Status :: 0 - Alpha",
+        "Framework :: YaPaPI",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Distributed Computing",
+    ],
+    python_requires=">=3.6.1",
+)
